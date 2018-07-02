9e4832fbfc44397d0dc08a5b32ebd6ed3530f1ba
Dylan Nirvana <dylan@dylannirvana.com>
2018-07-02 16:44:09 -0400

    adds custom-templates

---
+++ wp-content/themes/core-understrap-child/front-page.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Template Name: Empty Page Template
+*
+* Template for displaying a page just with the header and footer area and a "naked" content area in between.
+* Good for landingpages and other types of pages where you want to add a lot of custom markup.
+*
+* @package understrap
+*/
+
+get_header(); ?>
+
+<!-- These are the scenes -->
+  <!-- Photo -->
+  <?php get_template_part( "custom-templates/carousel") ?>
+
+  <!-- Photo -->
+  <?php get_template_part( "custom-templates/album") ?>
+
+  <!-- END scenes  -->
+
+<?php get_footer(); ?>
